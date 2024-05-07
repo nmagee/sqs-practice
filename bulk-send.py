f4a41d9b579300bf53e57db8c86abc0e89bec3cf
Neal Magee <nem2p@virginia.edu>
2024-05-07 09:55:35 -0400

    Tracking

--- bulk-send.py
+++ bulk-send.py
@@ -1,7 +1,5 @@
 import boto3
 from botocore.exceptions import ClientError
-import os
-import json
 
 def send_bulk(uid):
     sqs = boto3.client('sqs')
@@ -29,11 +27,10 @@ def send_bulk(uid):
             print(e)
     print(url)
     print(uid)
-    print("Bulk message sent")
+    print("Bulk messages sent")
 
-file1 = open('students.txt', 'r')
-Lines = file1.readlines()
-
+# file1 = open('students.txt', 'r')
+# Lines = file1.readlines()
 # for line in Lines:
 #     response = send_bulk(line)
 
--- get-message.py
+++ get-message.py
@@ -4,8 +4,10 @@ import requests
 import json
 
 # Set up your SQS queue URL and boto3 client
-url = "https://sqs.us-east-1.amazonaws.com/440848399208/xxxxxxx"
+url = "https://sqs.us-east-1.amazonaws.com/440848399208/nem2p"
 sqs = boto3.client('sqs')
+
+answer_dict = {}
 
 def delete_message(handle):
     try:
@@ -18,40 +20,54 @@ def delete_message(handle):
     except ClientError as e:
         print(e.response['Error']['Message'])
 
-def get_message():
-    try:
-        # Receive message from SQS queue. Each message has two MessageAttributes: order and word
-        # You want to extract these two attributes to reassemble the message
-        response = sqs.receive_message(
-            QueueUrl=url,
-            AttributeNames=[
-                'All'
-            ],
-            MaxNumberOfMessages=1,
-            MessageAttributeNames=[
-                'All'
-            ]
-        )
-        # Check if there is a message in the queue or not
-        if "Messages" in response:
-            # extract the two message attributes you want to use as variables
-            order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
-            word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
-            handle = response['Messages'][0]['ReceiptHandle']
+def get_messages():
+    for m in range(0, 10):
+        try:
+            # Receive message from SQS queue. Each message has two MessageAttributes: order and word
+            # You want to extract these two attributes to reassemble the message
+            response = sqs.receive_message(
+                QueueUrl=url,
+                AttributeNames=[
+                    'All'
+                ],
+                MaxNumberOfMessages=1,
+                MessageAttributeNames=[
+                    'All'
+                ]
+            )
+            # Check if there is a message in the queue or not
+            if "Messages" in response:
+                # extract the two message attributes you want to use as variables
+                order = response['Messages'][0]['MessageAttributes']['order']['StringValue']
+                word = response['Messages'][0]['MessageAttributes']['word']['StringValue']
+                handle = response['Messages'][0]['ReceiptHandle']
 
-            # Print the message attributes - this is what you want to work with to reassemble the message
-            print(f"Order: {order}")
-            print(f"Word: {word}")
+                # Print the message attributes - this is what you want to work with to reassemble the message
+                # print(f"Order: {order}")
+                # print(f"Word: {word}")
 
-        # If there is no message in the queue, print a message and exit    
-        else:
-            print("No message in the queue")
-            exit(1)
+                answer = {}
+                answer[order] = word
+                answer_dict.update(answer)
+
+            # If there is no message in the queue, print a message and exit    
+            else:
+                print("No message in the queue")
             
-    # Handle any errors that may occur connecting to SQS
-    except ClientError as e:
-        print(e.response['Error']['Message'])
+        # Handle any errors that may occur connecting to SQS
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+
+    # Sort the reassembled dict
+    sorted_dict = dict(sorted(answer_dict.items()))
+
+    answer = ""
+
+    for key in sorted_dict:
+        answer += sorted_dict[key] + " "
+
+    print(answer)
 
 # Trigger the function
 if __name__ == "__main__":
-    get_message()
+    get_messages()
